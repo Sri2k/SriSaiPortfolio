518014c3def65ee40a344e6a6e837b2126bf0bb8
SriSai_G <gandusrisai@gmail.com>
2023-08-22 17:15:09 +0530

    Layout component added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './components/layout/layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent, // Use the layout component as the root
+    children: [
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      // { path: 'home', component: HomeComponent },
+      // Add other routes...
+    ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
