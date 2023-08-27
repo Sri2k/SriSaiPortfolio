01d10ed1303d07d660aca58c28562ce2828f4251
SriSai_G <gandusrisai@gmail.com>
2023-08-27 14:00:53 +0530

    added complete cmd cases, added projectsMapping interface

--- src/app/components/command-prompt/command-prompt.component.ts
+++ src/app/components/command-prompt/command-prompt.component.ts
@@ -2,43 +2,56 @@ import { NavigationEnd, NavigationStart, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { Stack } from 'src/app/helpers/Stack';
 import { ContactsMapping } from 'src/app/interfaces/ContactsMapping ';
+import { ProjectsMapping } from 'src/app/interfaces/ProjectsMapping';
 
 @Component({
   selector: 'app-command-prompt',
   templateUrl: './command-prompt.component.html',
-  styleUrls: ['./command-prompt.component.scss']
+  styleUrls: ['./command-prompt.component.scss'],
 })
-export class CommandPromptComponent  implements OnInit{
+export class CommandPromptComponent implements OnInit {
   input: string = '';
   output: string = '';
   // toload:boolean = false;
-  masteravailableCommands: string[] = ['home','skills', 'help','testimonals','projects','contact'];
+  masteravailableCommands: string[] = [
+    'home',
+    'skills',
+    'help',
+    'testimonals',
+    'projects',
+    'contact',
+  ];
 
-  availableCommands: string[] = ['home','skills', 'help','testimonals'];
-  //availableProjectsCommands:string[] = ['tictoctoe']
-  availableProjectsCommands:string[] = ['tictoctoe']
-
-  availableContactsCommands:string[] = ['email','linkedin','instagram']
- availableContactsMapping: ContactsMapping = {
-    "email":"gandusrisai@gmail.com",
-    "linkedin": "https://www.w3schools.com/",
-    "instagram": "https://www.w4schools.com/"
+  availableCommands: string[] = ['home', 'skills', 'help', 'testimonals'];
+  availableProjectsCommands: string[] = ['tictoctoe', 'w3schools'];
+  availableProjectsMapping: ProjectsMapping = {
+    tictoctoe: 'tictoctoe',
+    w3schools: 'https://www.w3schools.com/',
   };
-  
+
+  availableContactsCommands: string[] = ['email', 'linkedin', 'instagram'];
+  availableContactsMapping: ContactsMapping = {
+    email: 'gandusrisai@gmail.com',
+    linkedin: 'https://www.w3schools.com/',
+    instagram: 'https://www.w4schools.com/',
+  };
 
   isOpen: boolean = true;
-  previousInput:string='';
+  previousInput: string = '';
   inputsStack: Stack<string> = new Stack<string>();
   historyIndex: number = -1;
- formattedMasterCommands = this.masteravailableCommands.map(command => `[${command}]`);
- formattedCommands = this.availableCommands.map(command => `[${command}]`);
- formattedProjectCommands = this.availableProjectsCommands.map(command => `[${command}]`);
- formattedContactCommands = this.availableContactsCommands.map(command => `[${command}]`);
+  formattedMasterCommands = this.masteravailableCommands.map(
+    (command) => `[${command}]`
+  );
+  formattedCommands = this.availableCommands.map((command) => `[${command}]`);
+  formattedProjectCommands = this.availableProjectsCommands.map(
+    (command) => `[${command}]`
+  );
+  formattedContactCommands = this.availableContactsCommands.map(
+    (command) => `[${command}]`
+  );
 
-
- constructor(private router: Router) {
-  
-}
+  constructor(private router: Router) { }
   ngOnInit(): void {
     // this.router.events.subscribe((routerEvent:any)=>{
     //   if(routerEvent instanceof NavigationStart){
@@ -48,95 +61,94 @@ export class CommandPromptComponent  implements OnInit{
     //     this.toload =false;
     //   }
     // })
-  
   }
 
   handleCommand() {
     const command = this.input.trim().toLowerCase();
     this.inputsStack.push(command);
-    if(command == 'help'){
-      this.output = `Available commands:\n${this.masteravailableCommands.join(', ')}`;
-    }
-    else if(this.availableCommands.includes(command)){
+    if (command == 'help') {
+      this.output = `Available commands:\n${this.masteravailableCommands.join(
+        ', '
+      )}`;
+    } else if (this.availableCommands.includes(command)) {
       this.output = `Redirecting to ${command} url...`;
       this.router.navigate([command]);
-      console.log(this.isOpen)
+      console.log(this.isOpen);
       this.isOpen = false;
-    //  this.router.events.subscribe((event) => {
-    //     if (event instanceof NavigationEnd) {
-    //         // Navigation has completed, update the message
-    //         this.isOpen = false;
-    //     }
-    // });
-    }
-    else if(command=='projects'){
-      this.output = `Available project commands: ${this.formattedProjectCommands.join(', ')}`;
-    }
-    else if(this.previousInput=='projects')
-    {
-      if(this.availableProjectsCommands.includes(command)){
-        this.output = `Redirecting to ${command} url...`;
-      this.router.navigate(['projects',command]);
-      this.isOpen = false;
-      // this.router.events.subscribe((event) => {
-      //   if (event instanceof NavigationEnd) {
-      //       // Navigation has completed, update the message
-      //       this.isOpen = false;
-      //   }
-      // });
-      }
-      else {
+    } else if (command == 'projects') {
+      this.output = `Available project commands: ${this.formattedProjectCommands.join(
+        ', '
+      )}`;
+    } else if (this.previousInput == 'projects') {
+      if (this.availableProjectsCommands.includes(command)) {
+        const linkOrRoute = this.availableProjectsMapping[command];
+
+        if (
+          linkOrRoute.startsWith('http://') ||
+          linkOrRoute.startsWith('https://')
+        ) {
+          // Open external links in a new tab/window
+          window.open(linkOrRoute, '_blank');
+        } else {
+          // Navigate to routing commands
+          this.router.navigate(['projects', linkOrRoute]);
+        }
+        this.isOpen = false;
+      } else {
         this.output = `Command not recognized: ${command}\nType 'help' for a list of commands.`;
       }
-    }
-    else if(command=='contact'){
-      this.output = `Available contact commands: ${this.formattedContactCommands.join(', ')}`;
-    }
-    else if(this.previousInput=='contact')
-    {
-      if(this.availableContactsCommands.includes(command)){
-      this.output = `Redirecting to ${command} url...`;
-      for (const key in this.availableContactsMapping) {
-        if (this.availableContactsMapping.hasOwnProperty(key)) {
-          window.open(this.availableContactsMapping[command], "_blank");
+    } else if (command == 'contact') {
+      this.output = `Available contact commands: ${this.formattedContactCommands.join(
+        ', '
+      )}`;
+    } else if (this.previousInput == 'contact') {
+      if (this.availableContactsCommands.includes(command)) {
+        this.output = `Redirecting to ${command} url...`;
+        for (const key in this.availableContactsMapping) {
+          if (this.availableContactsMapping.hasOwnProperty(key)) {
+            window.open(this.availableContactsMapping[command], '_blank');
+          }
         }
+        this.isOpen = false;
+      } else {
+        this.output = `Command not recognized: ${command}\nType 'help' for a list of commands.`;
       }
-      this.isOpen = false;
-    }
-    else {
+    } else {
       this.output = `Command not recognized: ${command}\nType 'help' for a list of commands.`;
     }
-    }
-    else {
-      this.output = `Command not recognized: ${command}\nType 'help' for a list of commands.`;
-    }
-    
-    
-    this.previousInput=command
+
+    this.previousInput = command;
     this.input = '';
   }
+
+  
   navigateHistory(direction: 'up' | 'down'): void {
     if (this.inputsStack.isEmpty()) return;
-  
+
     if (direction === 'up') {
-      this.historyIndex = Math.min(this.historyIndex + 1, this.inputsStack.size() - 1);
+      this.historyIndex = Math.min(
+        this.historyIndex + 1,
+        this.inputsStack.size() - 1
+      );
     } else if (direction === 'down') {
       this.historyIndex = Math.max(this.historyIndex - 1, -1);
     }
-  
+
     if (this.historyIndex === -1) {
       this.input = '';
     } else {
       this.input = this.inputsStack.items[this.historyIndex] || '';
     }
   }
-  
-
 
   closeCommandPrompt() {
     this.isOpen = !this.isOpen; // Set isOpen to false to hide the component
   }
-
- 
 }
 
+//  this.router.events.subscribe((event) => {
+//     if (event instanceof NavigationEnd) {
+//         // Navigation has completed, update the message
+//         this.isOpen = false;
+//     }
+// });
---
+++ src/app/interfaces/ProjectsMapping.ts
@@ -0,0 +1,4 @@
+export interface ProjectsMapping {
+    [key: string]: string;
+  }
+  
